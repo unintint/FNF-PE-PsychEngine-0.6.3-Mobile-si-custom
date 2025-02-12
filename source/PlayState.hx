4d48808ccd8cf22fca538c666dfe8d71009c6263
unintint <3137224039@qq.com>
2025-02-13 00:55:19 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3184,7 +3184,7 @@ class PlayState extends MusicBeatState
 					if(ClientPrefs.timeBarType == 'Time Left') timeTxt.text = FlxStringUtil.formatTime(secondsTotal, false);
 					if(ClientPrefs.timeBarType == 'SongName + Time Left') 
 					{
-						timeTxt.text = '(' + SONG.song + ')' + FlxStringUtil.formatTime(secondsTotal, false);
+						timeTxt.text = SONG.song + '(' + FlxStringUtil.formatTime(secondsTotal, false) + ')';
 						timeBar.color=FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]);
 					}
 				}
