e0d265a8c4dd09d2bb4893389922cea482ec5be5
unintint <3137224039@qq.com>
2025-02-13 00:57:10 +0800

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -106,6 +106,7 @@ class FPS extends TextField
 			#if openfl
 			memoryMegas = Math.abs(FlxMath.roundDecimal(#if cpp cpp.vm.Gc.memInfo64(cpp.vm.Gc.MEM_INFO_USAGE) #else System.totalMemory #end / 1000000, 1));
 			text += "\nMemory: " + memoryMegas + " MB";
+			text += "\nTiny Edtion v";
 			#end
 
 			text += os;
